25e46f66f3c742de7a34423930913daa9e85049a
Alexander Ivanov <aivanov@informatica.ru>
2018-06-04 12:53:46 +0300

    tools nedb

---
+++ tools/db/nedb/index.js
@@ -0,0 +1,36 @@
+import Datastore from 'nedb';
+import path from 'path';
+import logger from 'tools/logger';
+const { debug, time } = logger('tools.nedb');
+
+var baseDbPath = path.join(process.cwd(), 'nedb');
+var globalForce = true;
+var AUTO_COMPACTION_INTERVAL = 120 * 1000; // мс
+
+export let db = {};
+
+export const init = ({ modules }) =>
+  new Promise((resolve, reject) => {
+    Object.keys(modules).forEach(n => {
+      debug('modules key', modules[n]);
+      if (modules[n] && modules[n].hasOwnProperty('nedb')) {
+        if (modules[n].nedb.hasOwnProperty('dbmodels')) {
+          modules[n].nedb.dbmodels.forEach(function(collection) {
+            db[collection] = new Datastore({
+              filename: path.join(baseDbPath, collection),
+              autoload: true,
+            });
+            db[collection].persistence.setAutocompactionInterval(
+              AUTO_COMPACTION_INTERVAL
+            );
+          });
+        }
+        if (modules[n].nedb.hasOwnProperty('dbseed')) {
+          // TODO globalSyncForce
+          modules[n].nedb.dbseed(db).then(resolve);
+        } else {
+          return resolve();
+        }
+      }
+    });
+  });
